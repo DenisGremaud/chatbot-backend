785797ef902eba6e0353c227f95c9aeb2a7b4b2e
denis.gremaud <denis.gremaud@etu.hesge.ch>
2024-12-02 18:15:29 +0100

    minor update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
-import { ChromaClient } from 'chromadb';
 
 async function bootstrap() {
-  const chromadbCLient = new ChromaClient({
-    path: 'http://chroma.railway.internal:8000',
-  });
-  const chromadbversion = await chromadbCLient.version();
-  console.log('Chromadb version:', chromadbversion);
   const app = await NestFactory.create(AppModule);
   app.use(
     cors({
