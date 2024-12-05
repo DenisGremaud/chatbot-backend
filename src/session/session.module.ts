387a84b0e49033814b3cec218b5e91c4595aefd0
denis.gremaud <denis.gremaud@etu.hesge.ch>
2024-12-05 17:33:08 +0100

    update

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SessionManagerService } from './session-manager/session-manager.service';
 import { SessionManagerController } from './session-manager/session-manager.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [SessionManagerService],
   controllers: [SessionManagerController],
   exports: [SessionManagerService], // Export the service to be used in other modules
